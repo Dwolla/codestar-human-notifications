6710f465515c3006dba162e6b1fea467e87da8ca
Brian P. Holt <bholt@dwolla.com>
2020-03-18 15:55:52 -0500

    look up email addresses in environment variables

--- src/main/scala/com/dwolla/codestar/notifications/LambdaHandler.scala
+++ src/main/scala/com/dwolla/codestar/notifications/LambdaHandler.scala
@@ -1,5 +1,6 @@
 package com.dwolla.codestar.notifications
 
+import cats._
 import cats.effect._
 import cats.implicits._
 import com.amazonaws.services.lambda.runtime.Context
@@ -14,20 +15,23 @@ import natchez.Trace
 class LambdaHandler(printer: Printer) extends IOLambda[Json, Unit](printer) {
   def this() = this(Printer.noSpaces)
 
-//  val fromAddress = "noreply@dwolla.com"
-  val fromAddress = "dev-testing@dwolla.com"
-  val toAddress = "dev-testing@dwolla.com"
-
   override def handleRequestF[F[_] : Concurrent : ContextShift : Logger : Timer : Trace](blocker: Blocker)
                                                                                         (input: Json, context: Context): F[LambdaResponse[Unit]] =
-    SesAlg.resource[F].use { ses =>
-      for {
-        _ <- messagesInRecordsTraversal[CodeStarRecord](input).traverse_ {
-          case Notification(body, title) =>
-            ses.sendEmail(toAddress, fromAddress, title.getOrElse("CodeStar Notification"), body)
-          case UnparsableRecord(json) =>
-            ses.sendEmail(toAddress, fromAddress, "CodeStar Notification", json.spaces2)
-        }
-      } yield ()
-    }
+    SesAlg.resource[F]
+      .use(new Handler[F](_)(input))
+}
+
+class Handler[F[_]](ses: SesAlg[F]) {
+  def apply(input: Json)
+           (implicit E: Environment[F], F: Monad[F]): F[LambdaResponse[Unit]] =
+    for {
+      senderAddress <- Environment[F].get("SENDER_ADDRESS", "dev-testing@us-west-2.sandbox.dwolla.net")
+      recipientAddress <- Environment[F].get("SENDER_ADDRESS", "dev-testing@dwolla.com")
+      _ <- messagesInRecordsTraversal[CodeStarRecord](input).traverse_ {
+        case Notification(body, title) =>
+          ses.sendEmail(recipientAddress, senderAddress, title.getOrElse("CodeStar Notification"), body)
+        case UnparsableRecord(json) =>
+          ses.sendEmail(recipientAddress, senderAddress, "CodeStar Notification", json.spaces2)
+      }
+    } yield ()
 }
